c11f6528fd36dd1aa0c299c5ec4ac29b18f4a103
Michael Kellar <michael.d.kellar@gmail.com>
2024-12-17 17:46:14 -0800

    json file for text

--- src/Components/HomePage.tsx
+++ src/Components/HomePage.tsx
@@ -1,7 +1,11 @@
+import textJson from '../assets/site_text.json';
+
 const HomePage = () => {
+  const homePageText = textJson.home
+  const { title, subtitle, description } = homePageText
   return (
     <div>
-      <h1>Home Page</h1>
+      <h1>{title}</h1>
     </div>
   );
 };
