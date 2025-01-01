69a70f912bcc3e713e051c7cb1731fb47c141cce
Michael Kellar <michael.d.kellar@gmail.com>
2025-01-01 13:29:35 -0800

    using a blur for hpoto on main screen

--- src/Components/HomePage.tsx
+++ src/Components/HomePage.tsx
@@ -1,17 +1,40 @@
 import textJson from '../assets/site_text.json';
-import thinkingImageBW from '../assets/thinkingBW.jpg'; // Ensure you have the correct path to your image
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
 import TitleText from './TitleText';
-
+import useProgressiveImg from '../hooks/useProgressiveImg';
 
 const HomePage = () => {
   const homePageText = textJson.home
   const { title , subtitle , highlights } = homePageText
+
+  const BlurredUpImage = () => {
+    const {src, blur} = useProgressiveImg('/thinkingBW-low.png', '/thinkingBW.jpg');
+    console.log('debugImg',{src, blur});
+    return (
+      <img
+        src='/thinkingBW.jpg'
+        style={{
+          maxHeight:600,
+          maxWidth:"90%",
+          filter: blur ? "blur(20px)" : "none",
+          transition: blur ? "none" : "filter 0.1s ease-out"
+        }}
+      />
+    );
+  };
+
+
   return (
     <div className='homeBody'>
       <div className='homeImage'>
-        <img style={{maxHeight:600,maxWidth:"90%"}} src={thinkingImageBW} alt='Me' />
+        {/* <img style={{maxHeight:600,maxWidth:"90%"}} src={thinkingImageBW} alt='Me' /> */}
+        {/* <img 
+          style={{maxHeight:600,maxWidth:"90%"}} 
+          src= '/thinkingBW.jpg'
+          alt='Me' 
+        /> */}
+        <BlurredUpImage />
       </div>
       <div className='homeTitle'>
         <TitleText title={title} subTitle={subtitle} />
