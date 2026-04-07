07d4c6696d7cc545cb4df718d259d26cebc2ef7d
Michael Kellar <michael.d.kellar@gmail.com>
2026-04-07 14:58:34 -0700

    fix dependency with test suite

--- src/Components/NavigationBar.test.tsx
+++ src/Components/NavigationBar.test.tsx
@@ -1,28 +1,34 @@
-import { render, screen, fireEvent } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import '@testing-library/jest-dom'
 import NavigationBar from './NavigationBar';
 import { Page } from '../App';
 
-const allPages: Page[] = ['Home', 'About', 'Projects', 'Contact'] as const;
+const navPages: Page[] = ['About', 'Projects', 'Contact'] as const;
 
 describe('NavigationBar', () => {
   it('renders all navigation buttons', () => {
     const setPage = jest.fn();
-    render(<NavigationBar setPage={setPage} allPages={allPages} />);
+    const { getByText } = render(<NavigationBar setPage={setPage} allPages={['Home', ...navPages]} />);
 
-    allPages.forEach((page) => {
-      expect(screen.getByText(page)).toBeInTheDocument();
+    navPages.forEach((page) => {
+      expect(getByText(page)).toBeInTheDocument();
     });
+
+    expect(getByText('Michael Kellar')).toBeInTheDocument();
   });
 
   it('calls setPage with the correct argument when a button is clicked', () => {
     const setPage = jest.fn();
-    render(<NavigationBar setPage={setPage} allPages={allPages} />);
+    const { getByText } = render(<NavigationBar setPage={setPage} allPages={['Home', ...navPages]} />);
 
-    allPages.forEach((page) => {
-      const button = screen.getByText(page);
-      fireEvent.click(button);
+    navPages.forEach((page) => {
+      const button = getByText(page);
+      button.click();
       expect(setPage).toHaveBeenCalledWith(page);
     });
+
+    const homeLink = getByText('Michael Kellar');
+    homeLink.click();
+    expect(setPage).toHaveBeenCalledWith('Home');
   });
 });
